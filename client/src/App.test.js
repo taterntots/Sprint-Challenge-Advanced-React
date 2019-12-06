3b7e5c58a3443eebcce17cb5d240439a2069a188
taterntots <matthew.a.weidner@gmail.com>
2019-12-06 13:24:42 -0500

    answered the essay questions.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,3 +6,7 @@ test('renders without crashing', () => {
   render(<App />);
 })
 
+test('header title renders properly', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/Women's World Cup/i)).toBeTruthy()
+})

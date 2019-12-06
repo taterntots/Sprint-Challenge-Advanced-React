78c4c5bf3e75de955451e7fca4de913f2767d009
taterntots <matthew.a.weidner@gmail.com>
2019-12-06 11:27:15 -0500

    successfully received data from the api and set it to state.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor() {
@@ -8,6 +10,18 @@ class App extends React.Component {
       players: []
     }
   }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+    .then(response => {
+      console.log(response.data);
+      this.setState({ players: response.data });
+    })
+    .catch(error => {
+      console.log('I AM ERROR', error);
+    })
+  }
+
   render() {
     return (
       <div className="App">

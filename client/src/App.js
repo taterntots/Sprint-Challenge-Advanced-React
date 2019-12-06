bc47fa8980ece5c22929b520c467a0f7f0ab5339
taterntots <matthew.a.weidner@gmail.com>
2019-12-06 11:16:26 -0500

    restructured my App.js from a functional to a class component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Women's World Cup</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Women's World Cup</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
